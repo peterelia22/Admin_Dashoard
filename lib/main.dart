d284c40f1a65bb5a2994c9b715a2cf6816a21b4f
peterelia22 <peterelia914@gmail.com>
2025-10-28 22:05:01 +0300

    Connected project with Firebase

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:admin_dashboard/features/auth/presentation/views/signin_view.dart';
+import 'package:admin_dashboard/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    log('Firebase initialized');
+  } on Exception catch (e) {
+    log('Error initializing Firebase: $e');
+  }
+  runApp(const AdminDashboard());
+}
+
+class AdminDashboard extends StatelessWidget {
+  const AdminDashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SigninView());
+  }
+}
